49f6083373ee4510014300279884742e876eb81a
ccaioadriano <caiobhadriano@gmail.com>
2025-01-18 13:33:37 -0300

    iniciando projeto

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifólio</title>
+</head>
+
+<body>
+    <header>
+        <nav class="desktop">
+            <ul>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Serviços</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Serviços</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="banner-principal">
+        <form action="form.php">
+            <h2>Qual seu melhor e-mail ?</h2>
+            <input type="email">
+            <button type="submit">Cadastrar</button>
+        </form>
+    </section>
+
+    <section class="sobre">
+        <div class="w50">
+            <h2>Caio Adriano Rodrigues</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur quasi consequuntur nisi quaerat.
+                Facilis
+                nihil impedit doloribus quae! Dolore cupiditate debitis doloribus tempore sint aut non labore ad
+                explicabo
+                quos!</p>
+        </div>
+
+        <div class="w50">
+            <img src="public/images/perfil.jpg" alt="">
+        </div>
+
+    </section>
+</body>
+
+</html>
